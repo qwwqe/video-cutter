9471f744f6c7bc7926620100da29d9ac98e77bb2
qwwqe <f.werth@gmail.com>
2025-07-22 15:59:25 +0800

    Output EDL

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	// "math"
 	"math/rand"
 	"os"
 	"os/exec"
@@ -13,18 +12,11 @@ import (
 	"strings"
 )
 
-// func
-
 func main() {
-	// totalEditedTimeInSeconds := flag.Int("t", 300, "time of resulting edited video in seconds")
-	minimumShotTimeInSeconds := flag.Int("m", 3, "minimum length of any shot")
+	minimumShotTimeInSeconds := flag.Int("m", 7, "minimum length of any shot")
 	maximumShotTimeInSeconds := flag.Int("x", 20, "maximum length of any shot")
 	maximumCutsPerFile := flag.Int("f", 3, "maximum cuts taken for each file")
-
-	// useRandomOffset := flag.Bool("r", false, "use random offset for each clip")
 	outputFilePath := flag.String("o", "output.mp4", "output file path")
-	// tempDir := flag.String("t", "temp", "temp file directory")
-	// outputFile := flag.String("o", "output.mp4", "output file path")
 
 	flag.Parse()
 
@@ -32,20 +24,20 @@ func main() {
 		panic("Output file already exists")
 	}
 
-	commandsOutputFilePath := fmt.Sprintf(
-		"%s_commands.txt",
+	edlOutputFilePath := fmt.Sprintf(
+		"%s.edl",
 		strings.TrimSuffix(*outputFilePath, filepath.Ext(*outputFilePath)),
 	)
 
-	commandsOutputFile, err := os.OpenFile(commandsOutputFilePath, os.O_WRONLY|os.O_CREATE, 0644)
+	edlOutputFile, err := os.OpenFile(edlOutputFilePath, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		panic(err)
 	}
-	defer commandsOutputFile.Close()
+	defer edlOutputFile.Close()
 
-	// fmt.Println(*totalEditedTimeInSeconds)
-	// fmt.Println(*minimumShotTimeInSeconds)
-	// fmt.Println(*maximumShotTimeInSeconds)
+	if _, err = fmt.Fprintln(edlOutputFile, "# mpv EDL v0"); err != nil {
+		panic(err)
+	}
 
 	tempDir, err := os.MkdirTemp(".", "temp-")
 	if err != nil {
@@ -61,8 +53,6 @@ func main() {
 	}
 	defer concatFile.Close()
 
-	// videoTimes := make([]int, 0, len(filenames))
-	// totalVideoTime := 0
 	cutNumber := 0
 	for _, filename := range filenames {
 		decimalSecondsString, err := exec.Command(
@@ -94,7 +84,7 @@ func main() {
 				cutNumber,
 				tempDir,
 				concatFile,
-				commandsOutputFile,
+				edlOutputFile,
 			)
 
 			cutStart = cutEnd + *maximumShotTimeInSeconds
@@ -102,14 +92,6 @@ func main() {
 			cutsForFile++
 			cutNumber++
 		}
-		// if *useRandomOffset {
-		// 	cutOffset = rand.Intn(int(math.Max(float64(videoDurationInSeconds-cutTime), 1)))
-		// }
-
-		// if cutTime >= videoDurationInSeconds {
-		// 	cutTime = videoDurationInSeconds
-		// 	cutOffset = 0
-		// }
 	}
 
 	concatCommand := exec.Command(
@@ -126,22 +108,6 @@ func main() {
 		fmt.Println(string(output))
 		panic(err)
 	}
-
-	// for i, filename := range filenames {
-
-	// }
-
-	// averageClipTime := int(*totalEditedTimeInSeconds / len(videoTimes))
-
-	// if averageClipTime < *minimumShotTimeInSeconds {
-	// fmt.Printf("Average clip time ")
-	// }
-
-	// fmt.Printf("Total video time: %d\n", totalVideoTime)
-	// fmt.Printf("Average time per clip: %d\n", int(*totalEditedTimeInSeconds/len(videoTimes)))
-	// for i := range filenames {
-	// 	fmt.Printf("%s: %d seconds\n", filenames[i], videoTimes[i])
-	// }
 }
 
 func cutClip(
@@ -151,23 +117,10 @@ func cutClip(
 	clipNumber int,
 	tempDirPath string,
 	concatFile io.Writer,
-	commandsOutputFile io.Writer,
+	edlOutputFile io.Writer,
 ) {
 	cutOutputFile := fmt.Sprintf("%.04d-cut.mp4", clipNumber)
 	cutOutputPath := filepath.Join(tempDirPath, cutOutputFile)
-
-	// args := []string{}
-	// // if start > 0 && end >= videoDurationInSeconds {
-	// 	args = append(args,
-	// 		"-ss", fmt.Sprintf("%d", start),
-	// 		"-to", fmt.Sprintf("%d", end),
-	// 	)
-	// }
-	// args = append(args,
-	// 	"-i", filename,
-	// 	"-c", "copy",
-	// 	cutOutputPath,
-	// )
 
 	cutCommand := exec.Command(
 		"ffmpeg",
@@ -180,9 +133,12 @@ func cutClip(
 	)
 
 	fmt.Println(cutCommand)
-	fmt.Fprintln(commandsOutputFile, cutCommand.String())
 
 	if err := cutCommand.Run(); err != nil {
+		panic(err)
+	}
+
+	if _, err := fmt.Fprintf(edlOutputFile, "%s,%d,%d\n", inputFilename, start, end); err != nil {
 		panic(err)
 	}
 
